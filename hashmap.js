af8fc6cd9ae8004596f08379db39dbfd9e591556
Kayla Webb <kaylarwebb@gmail.com>
2018-01-29 12:24:38 -0500

    We can see the hash table values

--- hashmap.js
+++ hashmap.js
@@ -8,6 +8,15 @@ class HashMap {
     this._deleted = 0;
   }
 
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      throw new Error('Key error');
+    }
+    return this._slots[index].value;
+    
+  }
+
   static _hashString(string) {
     let hash = 5381;
     for (let i=0; i<string.length; i++) {
@@ -21,7 +30,7 @@ class HashMap {
   _findSlot(key) {
     const hash = HashMap._hashString(key);
     const start = hash % this._capacity;
-    for (let i=start; i < start.length; i++) {
+    for (let i=start; i < start + this._capacity; i++) {
       const index = i % this._capacity;
       const slot = this._slots[index];
       if (slot === undefined || (slot.key === key && !slot.deleted)) {
@@ -32,12 +41,12 @@ class HashMap {
   }
 
   set(key, value) {
-    const loadRatio = (this.length + 1) / this._capacity;
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
     if (loadRatio > HashMap.MAX_LOAD_RATIO) {
       this._resize(this._capacity * HashMap.SIZE_RATIO);
     }
     const index = this._findSlot(key);
-    //if the slot is not undefined, _findSlot(key) to find empty slot
+    //if the slot value is not empty, find next available slot to find empty slot
     this._slots[index] = {
       key, 
       value,
@@ -78,3 +87,20 @@ HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
 
 let lor = new HashMap;
+
+lor.set('Hobbit', 'Bilbo');
+lor.set('Hobbit', 'Frodo');
+lor.set('Wizard', 'Gandolf');
+lor.set('Human', 'Aragon');
+lor.set('Elf', 'Legolas');
+lor.set('Maiar', 'The Necromancer');
+lor.set('Maiar', 'Sauron');
+lor.set('RingBearer', 'Gollum');
+lor.set('LadyOfLight', 'Galadriel');
+lor.set('HalfElven', 'Arwen');
+lor.set('Ent', 'Treebeard');
+
+
+console.log('Lor hashmap>>', lor._slots);
+// console.log('Lor slot Hobbit>>', lor._findSlot('Hobbit'));
+console.log('Get hobbit>>>>', lor.get('Hobbit'));
